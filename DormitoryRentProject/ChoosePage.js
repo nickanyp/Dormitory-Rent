40676260fdfa3ca5cd05f9da260beda9b0b8f234
nickanyp <85357272+nickanyp@users.noreply.github.com>
2023-09-16 22:28:25 +0700

    icon

--- DormitoryRentProject/ChoosePage.js
+++ DormitoryRentProject/ChoosePage.js
@@ -6,22 +6,24 @@ import {
   TouchableOpacityBase,
 } from "react-native";
 import FirstPage from "./FirstPage";
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
 export default function ChoosePage() {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 20, fontWeight: "bold", color: "#363C56" }}>
         คุณเป็น<Text style={{ color: "#96B3FF" }}>เจ้าของหอพัก</Text>
-        หรือ<Text style={{color: "#FF9699"}}>ผู้เช่าหอพัก</Text>?
+        หรือ<Text style={{ color: "#FF9699" }}>ผู้เช่าหอพัก</Text>?
       </Text>
 
-      <TouchableOpacity
-        style={[styles.style, { backgroundColor: "#96B3FF", margin: 30 }]}
-      >
+      <TouchableOpacity style={[styles.style, { backgroundColor: "#96B3FF", margin: 30 }]}>
+        <FontAwesome5 name="hotel" size={24} color="black" />
         <Text style={styles.text}>เจ้าของหอพัก</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={[styles.style, { backgroundColor: "#FF9699" }]}>
+        <Ionicons name="people" size={24} color="black" />
         <Text style={styles.text}>ผู้เช่าหอพัก</Text>
       </TouchableOpacity>
     </View>
