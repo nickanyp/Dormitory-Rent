d127fc2a3e7c27199a8d5b99967bd00322b6bf98
nickanyp <85357272+nickanyp@users.noreply.github.com>
2023-09-30 00:28:59 +0700

    MyRoomPage

--- DormitoryRentProject/App.js
+++ DormitoryRentProject/App.js
@@ -2,13 +2,15 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import FirstPage from './MainScreen/FirstPage';
 import ChoosePage from './MainScreen/ChoosePage';
 import LoginRenter from './Renter/LoginRenter';
+import MyRoomPage from './Screen/Renter/MyRoomPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <FirstPage text='DÖrmitory Rent'></FirstPage> */}
       {/* <ChoosePage></ChoosePage> */}
-      <LoginRenter></LoginRenter>
+      {/* <LoginRenter></LoginRenter> */}
+      <MyRoomPage></MyRoomPage>
     </View>
   );
 }
---
+++ DormitoryRentProject/Screen/Renter/MyRoomPage.js
@@ -0,0 +1,75 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const MyRoomPage = () => {
+  return (
+    <View>
+      <View>
+        <View style={styles.box}>
+          <Text style={styles.text}>ชื่อผู้เช่า 1 : อนัญพร จอมคำ</Text>
+          <Text style={styles.text}>ชื่อผู้เช่า 2 : ตรียา เอื้อเจริญศรี</Text>
+          <Text></Text>
+          <Text style={styles.text}>หอพัก : กัลยรัตน์ 2</Text>
+          <Text style={styles.text}>เลขห้อง : 226</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={{ fontWeight: "bold", fontSize: 25, color: "#FF9699" }}>
+            กันยายน
+          </Text>
+          <Text></Text>
+          <Text style={styles.text}>ค่าเช่าหอพัก : 4600 บาท</Text>
+          <Text style={styles.text}>ค่าน้ำ : 150 บาท</Text>
+          <Text style={styles.text}>ค่าไฟ : 1200 บาท</Text>
+          <Text></Text>
+          <Text style={[styles.text, { color: "#FF9699" }]}>
+            รวมทั้งสิ้น : 5950 บาท
+          </Text>
+        </View>
+      </View>
+
+      <View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={[styles.textBtn, { color: "white", textAlign: "center" }]}>
+            ชำระค่าเช่า
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={[styles.textBtn, { color: "white", textAlign: "center" }]}>
+            ประวัติค่าเช่าหอ
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    width: 300,
+    borderWidth: 1.5,
+    borderRadius: 10,
+    borderColor: "#9B9B9B",
+    padding: 15,
+    margin: 10,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  btnContainer: {
+    backgroundColor: "red",
+  },
+  btn: {
+    width: 120,
+    height: 40,
+    borderRadius: 40,
+    backgroundColor: "#FF9699",
+    justifyContent: "center",
+    margin: 8,
+  },
+  textBtn: {
+    fontWeight: "bold",
+    fontSize: 13,
+  },
+});
+
+export default MyRoomPage;
